b323b7b5b63a47cd3d750dbab5e415949350e0f6
Jason  Rosso <jasonrosso@gmail.com>
2017-05-18 16:36:34 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,21 +13,41 @@ end
 def get_contestant_name(data, occupation)
 data.each do |season, season_array|
   season_array.each do |into_array|
-    if into_array[:occupation] == occupation
-      return into_array[:name]
+    if into_array["occupation"] == occupation
+      return into_array["name"]
       end
     end
   end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+ data.each do |season, season_array|
+    season_array.each do |into_array|
+      if into_array["hometown"] == hometown
+        counter+=1
+      end
+  end
+ end
+ return counter
 end
 
+
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, season_array|
+    season_array.each do |into_array|
+      if into_array["hometown"] == hometown
+        return into_array["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages =[]
+  data[season].each do |into_array|
+    ages << into_array["age"]
+  end
+  int_array =ages.map{|x| x.to_f}
+  return (int_array.inject{|x,sum| sum+=x}/int_array.size).round
 end
